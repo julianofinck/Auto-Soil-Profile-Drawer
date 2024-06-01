dd58c10a072a0f0e26ea56a846a7ff37dc5a0e87
Juliano Santos Finck <59181757+julianofinck@users.noreply.github.com>
2024-06-01 17:44:18 -0300

    feature: updated 3_run.bat

--- 3_run.bat
+++ 3_run.bat
@@ -1,2 +1,12 @@
-python -B "template\interpret.py"
+@REM Set the path to the Python interpreter
+set PYTHON="python"
+
+@REM Set the path to the main script
+set SCRIPT="template\translate_csv_2_acad_drawing.py"
+
+@REM Call the Python interpreter on the main script
+%PYTHON% -B %SCRIPT%
+
+@REM Wait for the user to press a key before closing the window
 pause
+
